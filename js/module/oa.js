c05703ecb64039c8b6ce511be7c55e023d03f046
Zz451117872 <451117872@qq.com>
2018-03-06 11:01:58 +0800

    1

--- js/module/oa.js
+++ js/module/oa.js
@@ -7,7 +7,7 @@
 
 
 
-var url_prefix = "http://localhost:8080/oa/";
-//var url_prefix = "http://120.79.50.238:8080/oa/";
+//var url_prefix = "http://localhost:8080/oa/";
+var url_prefix = "http://120.79.50.238:8080/oa/";
 
 
